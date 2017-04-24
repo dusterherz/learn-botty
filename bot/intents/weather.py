c5fb75f5889146b0439224d91bfe9545acbb55b2
Aude Lejuez <dusterherz@gmail.com>
2017-04-24 14:11:49 +0200

    Change location debugged ! 🌍 🌤
    + Can really ask for the meteo around the world
    - Choose only the last location if multiples are given

--- bot/intents/weather.py
+++ bot/intents/weather.py
@@ -4,18 +4,19 @@ import requests
 import settings
 
 def handle(response):
-    if 'location' in response.entities:
-        lat = response.entities.location.lat
-        lng = response.entities.location.lng
-    else:
-        lat = '50.6292500'
-        lng = '3.0572560'
+    lat = '50.6292500'
+    lng = '3.0572560'
+    # Take only the last location
+    for entity in response.entities:
+        if entity.name == 'location':
+            lat = str(entity.lat)
+            lng = str(entity.lng)
     if 'datetime' in response.entities:
         date = dateutils.parser.parse(response.entities.datetime[0].iso)
     else:
         date = datetime.today()
     date.replace(hour=9, minute=0, second=0, microsecond=0)
-
+    print(lat + ', lng : ' + lng)
     # Use of DarkSkyApi to get the meteo. Change API if the date is today
     result = requests.get('https://api.darksky.net/forecast/' +
         settings.config['darkSky']['apiKey'] +
