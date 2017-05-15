aa3a92417c7e83dca5b3d3466b5d1410c551d661
Aude Lejuez <dusterherz@gmail.com>
2017-05-15 14:53:22 +0200

    clean code with linter

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 
 import os
 import settings
@@ -10,9 +10,11 @@ from bot import bot
 app = Flask(__name__)
 settings.init()
 
+
 @app.route("/", methods=['POST'])
 def root():
-  return bot(request)
+    return bot(request)
+
 
 if __name__ == '__main__':
     app.run(port=settings.config['flask']['port'])
